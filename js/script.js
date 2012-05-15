860c54bdfc212bb87c96b842eb210ee502f74c84
Michael <mltroutt@gmail.com>
2012-05-15 14:07:54 -0500

    Added keyboad selection

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 (function($) {
 	
 	$("#search-field").keydown(function(event) {
-		if((event.which == 38 || event.which == 40) && !$('#popup').is(':hidden') && $('#popup').has('p.search-link').length != 0) {
-			
+		if((event.which == 38 || event.which == 40 || event.which == 27) && !$('#popup').is(':hidden') && $('#popup').has('p.search-link').length != 0) {
+			switch(event.which) {
+				case 38:
+					if($('#popup').find('p.search-hovered').length == 0) {
+						$('#popup p.search-link').last().addClass("search-hovered");
+					} else {
+						$('#popup p.search-hovered').removeClass("search-hovered").prev().addClass("search-hovered");
+					}
+					break;
+				case 40:
+					if($('#popup').find('p.search-hovered').length == 0) {
+						$('#popup p.search-link').first().addClass("search-hovered");
+					} else {
+						$('#popup p.search-hovered').removeClass("search-hovered").next().addClass("search-hovered");
+					}
+					break;
+			}
 		}
 	});
 	
 	last_term = '';
-	$("#search-field").keyup(function() {
+	$("#search-field").keyup(function(event) {
 		//check if the current term is the same as the last
 		//prevents multiple searches due to multiple keys pressed at once
 		if(last_term != $("#search-field").val()){
@@ -48,5 +63,23 @@
 			$(this).removeClass("search-hovered");
 		}
 	});
+	
+	$(document).bind('keydown', function (event){
+		switch(event.which) {
+			case 13:
+				if ( !$('#popup').is(':hidden') ) {
+					$("#search-field").val($('#popup p.search-hovered').html());
+					$("#popup").hide();
+					return false;
+				}
+				break;
+			case 27:
+				if ( !$('#popup').is(':hidden') ) {
+					$('#popup').hide();
+				} 
+				break;
+			}
+  	});
+
 	
 })(jQuery);
