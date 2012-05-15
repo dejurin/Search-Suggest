769b05e691fbc7502c07f37da1cb4c73484f1811
Michael <mltroutt@gmail.com>
2012-05-15 14:10:56 -0500

    Some commenting

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@
 	$("#search-field").keydown(function(event) {
 		if((event.which == 38 || event.which == 40 || event.which == 27) && !$('#popup').is(':hidden') && $('#popup').has('p.search-link').length != 0) {
 			switch(event.which) {
+				//Down Arrow
 				case 38:
+					//If nothing is highlighted
 					if($('#popup').find('p.search-hovered').length == 0) {
 						$('#popup p.search-link').last().addClass("search-hovered");
 					} else {
+						//Remove the current highlight and add a highlight to the term above
 						$('#popup p.search-hovered').removeClass("search-hovered").prev().addClass("search-hovered");
 					}
 					break;
+				//Up Arrow
 				case 40:
+					//If nothing is highlighted
 					if($('#popup').find('p.search-hovered').length == 0) {
 						$('#popup p.search-link').first().addClass("search-hovered");
 					} else {
+						//Remove the current highlight and add a highlight to the term below
 						$('#popup p.search-hovered').removeClass("search-hovered").next().addClass("search-hovered");
 					}
 					break;
@@ -49,6 +55,7 @@
 	
 	
 	$(".search-link").live("click", function(event) {
+		//change the search field to the value that is clicked
 		$("#search-field").val($(this).html());
 		$("#popup").hide();
 	});
@@ -66,13 +73,16 @@
 	
 	$(document).bind('keydown', function (event){
 		switch(event.which) {
+			//Enter
 			case 13:
 				if ( !$('#popup').is(':hidden') ) {
+					//change the search value to the term currently highlighted
 					$("#search-field").val($('#popup p.search-hovered').html());
 					$("#popup").hide();
 					return false;
 				}
 				break;
+			//Escape
 			case 27:
 				if ( !$('#popup').is(':hidden') ) {
 					$('#popup').hide();
